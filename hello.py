241b6d7d2134402ead5341d277b92da4ba5bb319
RnRQueen <zubik1103@gmail.com>
2020-10-02 15:39:07 +0300

    strings

--- hello.py
+++ hello.py
@@ -17,3 +17,34 @@ print(result)
 year = 2020
 is_leap = (year % 4 == 0) and (year % 100 != 0 or year % 400 == 0)
 print(is_leap)
+
+import calendar
+print(calendar.isleap(2012))
+
+
+quote = '"Дайте мне точку опоры, и я переверну мир!"'
+print(quote.count("е"))
+print("мир".capitalize())
+
+print("1a1".isdigit())
+
+print("я переверну" in "Дайте мне точку опоры, и я переверну мир!")
+
+new_string = "Python"
+for letter in new_string:
+    print("Letter", letter)
+
+template = " И %s такой молодой, и юный %s впереди"
+print(template % ("Ленин", "октябрь"))
+print("Давайте {} паузы в {}, произнося и {} снова".format("делать", "словах", "замолкая"))
+
+subject = "елочка"
+action = "росла"
+print(f"В лесу родилась {subject}, в лесу она {action}")
+
+num = 6/7
+print(num)
+print(f"{num:.2f}")
+
+name = input("Input your name:")
+print(f"Hello, {name}!")
